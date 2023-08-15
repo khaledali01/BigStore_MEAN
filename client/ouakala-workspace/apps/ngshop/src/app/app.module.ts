bd7b3536b97401c689e226cc57b06b7e350286cd
Khaled <khaled.ali.architect@gmail>
2023-08-15 16:33:19 +0300

    Creating App Level Components - Routing

--- client/ouakala-workspace/apps/ngshop/src/app/app.module.ts
+++ client/ouakala-workspace/apps/ngshop/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product/product-list/product-list.component';
+import { PrimaryHeaderComponent } from './shared/primary-header/primary-header.component';
+import { PrimaryFooterComponent } from './shared/primary-footer/primary-footer.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, NxWelcomeComponent, HomePageComponent, ProductListComponent, PrimaryHeaderComponent, PrimaryFooterComponent],
+    imports: [BrowserModule, AppRoutingModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
